cc2984429f2e8591ea2cb76981be6370adbd5bd4
Alec Wantoch <alec@wantoch.net>
2019-07-24 16:50:21 -0400

    Updates

--- dapp.e8ecdbeb.js
+++ dapp.e8ecdbeb.js
@@ -7094,10 +7094,168 @@ module.exports = {
       "links": {},
       "address": "0x7909DDa961fB2D8532D12888E2E6c9fEAc65b300",
       "transactionHash": "0xc3e7daf54e9b379510ea83647b9149124a568e4f1e79261d5b83b547ceb40fc5"
+    },
+    "1563996041364": {
+      "events": {
+        "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "previousOwner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }],
+          "name": "OwnershipTransferred",
+          "type": "event",
+          "signature": "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"
+        },
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "from",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "to",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "tokenId",
+            "type": "uint256"
+          }],
+          "name": "Transfer",
+          "type": "event",
+          "signature": "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+        },
+        "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "approved",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "tokenId",
+            "type": "uint256"
+          }],
+          "name": "Approval",
+          "type": "event",
+          "signature": "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"
+        },
+        "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "operator",
+            "type": "address"
+          }, {
+            "indexed": false,
+            "name": "approved",
+            "type": "bool"
+          }],
+          "name": "ApprovalForAll",
+          "type": "event",
+          "signature": "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31"
+        }
+      },
+      "links": {},
+      "address": "0x7909DDa961fB2D8532D12888E2E6c9fEAc65b300",
+      "transactionHash": "0xc3e7daf54e9b379510ea83647b9149124a568e4f1e79261d5b83b547ceb40fc5"
+    },
+    "1563996625182": {
+      "events": {
+        "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "previousOwner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "newOwner",
+            "type": "address"
+          }],
+          "name": "OwnershipTransferred",
+          "type": "event",
+          "signature": "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"
+        },
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "from",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "to",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "tokenId",
+            "type": "uint256"
+          }],
+          "name": "Transfer",
+          "type": "event",
+          "signature": "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+        },
+        "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "approved",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "tokenId",
+            "type": "uint256"
+          }],
+          "name": "Approval",
+          "type": "event",
+          "signature": "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"
+        },
+        "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31": {
+          "anonymous": false,
+          "inputs": [{
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          }, {
+            "indexed": true,
+            "name": "operator",
+            "type": "address"
+          }, {
+            "indexed": false,
+            "name": "approved",
+            "type": "bool"
+          }],
+          "name": "ApprovalForAll",
+          "type": "event",
+          "signature": "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31"
+        }
+      },
+      "links": {},
+      "address": "0x7909DDa961fB2D8532D12888E2E6c9fEAc65b300",
+      "transactionHash": "0xc3e7daf54e9b379510ea83647b9149124a568e4f1e79261d5b83b547ceb40fc5"
     }
   },
   "schemaVersion": "3.0.11",
-  "updatedAt": "2019-07-24T19:09:04.038Z",
+  "updatedAt": "2019-07-24T19:30:33.660Z",
   "devdoc": {
     "methods": {
       "approve(address,uint256)": {
